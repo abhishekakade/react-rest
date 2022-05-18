d5920eddfc2e2701d251b5d8f4c5d093a60a5c2a
abhishekakade <26276153+abhishekakade@users.noreply.github.com>
2022-05-18 23:16:20 +0530

    initial commit

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  // console.log(url);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [apiData, setApiData] = useState([]);
+  const [serverError, setServerError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        const resp = await axios.get(url);
+        const data = await resp?.data;
+
+        setApiData(data);
+        setIsLoading(false);
+      } catch (err) {
+        setServerError(err);
+        setIsLoading(false);
+        console.error(err);
+      }
+    };
+
+    fetchData();
+    // https://gorest.co.in/public/v1/users?page=1
+  }, [url]);
+
+  // console.log(launchInfo);
+  return { isLoading, apiData, serverError };
+};
+
+export default useFetch;
